c16b945b18d5c6a32b5d5d828ee8d27dec6c500c
R <50966008+rashmika13@users.noreply.github.com>
2020-06-26 17:48:02 -0400

    home page and about page added

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+
+
+def home(request):
+    return HttpResponse('<h1>Hello Finch Collectors</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')
