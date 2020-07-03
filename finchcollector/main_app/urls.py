4ef889921773bfe1525fc4df46117aece640b2dc
R <50966008+rashmika13@users.noreply.github.com>
2020-07-03 13:50:32 -0400

    auth added, delete photo added

--- finchcollector/main_app/urls.py
+++ finchcollector/main_app/urls.py
@@ -2,35 +2,47 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home and About
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-
+    # Finch list and detail
     path('finches/', views.finches_index, name='index'),
     path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
-
+    # Finch create , edit and delete
     path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
     path('fiches/<int:pk>/update/',
          views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/',
          views.FinchDelete.as_view(), name='finches_delete'),
-
+    # Finch feeding
 
     path('finches/<int:finch_id>/add_feeding/',
          views.add_feeding, name='add_feeding'),
-
+    # Finch photos
     path('finches/<int:finch_id>/add_photo/',
          views.add_photo, name='add_photo'),
+    #     path('photos/<int:pk>/delete/', views.PhotoDelete.as_view(),
+    #          name='photos_delete'),
+
+    path('finches/<int:finch_id>/photos/<int:photo_id>/delete/',
+         views.photos_delete, name='photos_delete'),
 
 
+    # Toy list and detail
     path('toys/', views.ToyList.as_view(), name='toys_index'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
-
+    # Toy create, update, delete
     path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
     path('toys/<int:pk>/update/',
          views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/',
          views.ToyDelete.as_view(), name='toys_delete'),
+
+    # Finch and Toy association
     path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/',
          views.assoc_toy, name='assoc_toy'),
+    # Signup
+    path('accounts/signup/', views.signup, name='signup'),
+
 
 ]
--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -3,6 +3,10 @@ from .models import Finch, Toy, Photo
 from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
 S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
@@ -31,35 +35,47 @@ def about(request):
     return render(request, 'about.html')
 
 
+@login_required
 def finches_index(request):
-    finches = Finch.objects.all()
+    finches = Finch.objects.filter(user=request.user)
     return render(request, 'finches/index.html', {'finches': finches})
 
 
-class FinchCreate(CreateView):
+class FinchCreate(LoginRequiredMixin, CreateView):
     model = Finch
-    fields = '__all__'
-    # success_url = '/cats/'
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 
-class FinchUpdate(UpdateView):
+class FinchUpdate(LoginRequiredMixin, UpdateView):
     model = Finch
     fields = ['breed', 'description', 'age']
 
 
-class FinchDelete(DeleteView):
+class FinchDelete(LoginRequiredMixin, DeleteView):
     model = Finch
     success_url = '/finches/'
 
 
+@login_required
 def finches_detail(request, finch_id):
-    finch = Finch.objects.get(id=finch_id)
-    feeding_form = FeedingForm()
+    try:
+        finch = Finch.objects.get(id=finch_id, user=request.user)
+    except:
+        return redirect('index')
+
     toys_finch_doesnt_have = Toy.objects.exclude(
         id__in=finch.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 
 
+@login_required
 def add_feeding(request, finch_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -69,6 +85,7 @@ def add_feeding(request, finch_id):
     return redirect('detail', finch_id=finch_id)
 
 
+@login_required
 def add_photo(request, finch_id):
     # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
@@ -90,30 +107,62 @@ def add_photo(request, finch_id):
     return redirect('detail', finch_id=finch_id)
 
 
-class ToyList(ListView):
+@login_required
+def photos_delete(request, finch_id, photo_id):
+    p = Photo.objects.get(pk=photo_id)
+    p.delete()
+    return redirect('detail', finch_id=finch_id)
+
+# class PhotoDelete(DeleteView):
+#     model = Photo
+#     success_url = '/finches/'
+
+
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
 
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
 
 
+@login_required
 def assoc_toy(request, finch_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     Finch.objects.get(id=finch_id).toys.add(toy_id)
     return redirect('detail', finch_id=finch_id)
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
